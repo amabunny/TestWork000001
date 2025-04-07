f26d21fc1748c1aae87c4190bd87b908cb59fbb2
Sergey <iamgreyy@gmail.com>
2025-04-07 15:33:08 +0500

    feat(*): initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      "plugin:prettier/recommended",
+    ],
+    plugins: ["simple-import-sort", "prettier"],
+  }),
 ];
 
 export default eslintConfig;
